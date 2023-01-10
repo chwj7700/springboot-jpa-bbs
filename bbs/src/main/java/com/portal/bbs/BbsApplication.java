9a59d5de681b56c32348f3a52c6a52983b5ee215
chwj7700 <chwj7700@naver.com>
2023-01-11 00:43:33 +0900

    auditing 활성화

--- bbs/src/main/java/com/portal/bbs/BbsApplication.java
+++ bbs/src/main/java/com/portal/bbs/BbsApplication.java
@@ -2,8 +2,11 @@ package com.portal.bbs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BbsApplication {
 
 	public static void main(String[] args) {
